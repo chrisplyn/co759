56a8f27528e67d742f18c5a625b4fd8b36e529e7
chrisplyn <y652liu@uwaterloo.ca>
2015-03-30 15:55:01 -0400

    add histogram and scatter plot

--- result.r
+++ result.r
@@ -1,13 +1,22 @@
 
-require(vcd)
-require(MASS)
+
 
 running_time <- read.table("time",header=FALSE)
 
+running_time <- running_time$V1
 
 
-# plot a graph
-hist(running_time, freq = FALSE, breaks = 500, xlim = c(0, quantile(running_time, 0.99)))
-plot(running_time)
+
+setEPS()
+postscript("histogram.eps")
+hist(running_time, freq = FALSE, breaks = 500, xlim = c(0, quantile(running_time, 0.99)), main="Histogram of running time")
+lines(density(running_time), col="red") 
+dev.off()
+
+setEPS()
+postscript("scatter plot.eps")
+plot(running_time,xlab="iteration", ylab="running time", main="scatter plot of running time")
+abline(h=mean(running_time), col="red", lty="dotted")
+dev.off()
 
 summary(running_time)
